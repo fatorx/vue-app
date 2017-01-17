ce5e5064e8a9c146db64e208161f9ba654e36cc8
Rassis7 <romuloassis.as@gmail.com>
2017-01-17 00:13:41 -0200

    Primeiro commit
    …

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+
+      <hello></hello>
+
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,35 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+import VueMaterial from 'vue-material'
+import VueRouter from 'vue-router'
+import 'vue-material/dist/vue-material.css'
+
+import Hello from './components/Hello.vue'
+import AboutUs from './components/AboutUs.vue'
+
+/* iniciar o material */
+Vue.use(VueMaterial)
+/* iniciar as rotas */
+Vue.use(VueRouter)
+
+/* tema default */
+Vue.material.registerTheme({ Primary: { color: 'indigo', hue: 'A200' } })
+
+/* configurando rotas */
+const routers = [
+    { path: '/', redirect: '/hello' },
+    { path: '/hello', component: Hello },
+    { path: '/about-us', component: AboutUs }
+]
+
+/* mode: 'history' (serve para tirar o /#/ da url) */
+const router = new VueRouter({ routers })
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
